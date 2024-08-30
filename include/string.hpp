a853a46c9e8e76528d2a2aaf0eb08c566ff93627
DolphyWind <yunusaydin590@gmail.com>
2024-08-30 23:06:41 +0300

    Add Doxygen documentation and test coverage

--- include/string.hpp
+++ include/string.hpp
@@ -6,15 +6,44 @@
 namespace yacw
 {
 
+/**
+ * @brief String of integers instead of chars, since ncurses uses ints in character-related functions.
+ */
 class string : public std::basic_string<int>
 {
 public:
+    /**
+     * @brief Construct a yacw::string from a const char*
+     *
+     * @param str const char* to construct yacw::string from
+     */
     explicit string(const char* str);
+
+    /**
+     * @brief Construct a yacw::string from a std::string 
+     *
+     * @param str std::string object to construct yacw::string from
+     */
     explicit string(const std::string& str);
 
+    /**
+     * @brief Converts yacw::string to a std::string by casting each yacw::string character to char
+     *
+     */
     std::string to_cppstring() const;
 
+    /**
+     * @brief Applies bitwise OR operation to each character
+     *
+     * @param x Number to bitwise OR with
+     * @return The result of bitwise OR operation
+     */
     string operator|(int x) const;
+    /**
+     * @brief Applies bitwise OR operation to each character
+     *
+     * @param x Number to bitwise OR with
+     */
     string& operator|=(int x);
     string& operator=(const char* str);
     string& operator=(const std::string& str);
@@ -37,6 +66,10 @@ public:
 
     std::strong_ordering operator<=>(const string& str) const;
 
+    /**
+     * @brief Convert yacw::string to std::string by casting each yacw::string character to char
+     *
+     */
     operator std::string() const;
 };
 
--- tests/yacw_stringtest.cpp
+++ tests/yacw_stringtest.cpp
@@ -1,10 +1,11 @@
 #include <cassert>
 #include <curses.hpp>
 #include <yacw.hpp>
-using namespace yacw::string_literals;
 
 int main()
 {
+    using namespace yacw::string_literals;
+    using namespace std::string_literals;
     yacw::string s = "Hello"_ys | A_BOLD;
     
     assert(s == std::string("Hello"));
@@ -17,7 +18,18 @@ int main()
     s.push_back('A');
     assert(s == "HelloA");
     s.pop_back();
-    assert(s.to_cppstring() == std::string("Hello"));
+    assert(s.to_cppstring() == "Hello"s);
+    assert("Hello" == (std::string)"Hello"_ys);
+    assert("Hello"s < "Helloo"_ys);
+    assert("Hello"s <= "Helloo"_ys);
+    assert("Hello"s == "Hello"_ys);
+    assert("Helloo"s >= "Helloo"_ys);
+    assert("Helloo"s > "Hello"_ys);
+    assert("Helloo"_ys > "Hello"s);
+    assert("Helloo"_ys >= "Helloo"s);
+    assert("Hello"_ys <= "Hello"s);
+    assert("a"s != "b"_ys);
+    assert(std::is_eq("a"s <=> "a"_ys));
 
     return 0;
 }
